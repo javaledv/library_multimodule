e8e18a459e53fa21f9ff03f46d546fb4c57b7f59
dzianis.leshaniuk <dzianis.leshaniuk@instinctools.ru>
2018-01-20 10:32:30 +0300

    Added entities.

---
+++ mongodb_module/src/main/java/ml/ledv/library/modules/db/entity/impl/PublisherEntity.java
@@ -0,0 +1,35 @@
+package ml.ledv.library.modules.db.entity.impl;
+
+import ml.ledv.library.modules.db.entity.BaseEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity(name = "PUBLISHER")
+public class PublisherEntity extends BaseEntity {
+
+    @Column(name = "name")
+    private String name;
+
+    public PublisherEntity() {
+    }
+
+    public PublisherEntity(final String id) {
+        super(id);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "PublisherEntity{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ mongodb_module/src/main/java/ml/ledv/library/modules/db/entity/impl/UserEntity.java
@@ -0,0 +1,47 @@
+package ml.ledv.library.modules.db.entity.impl;
+
+import ml.ledv.library.modules.db.entity.BaseEntity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name = "USER")
+public class UserEntity extends BaseEntity {
+
+    @Column(name = "name")
+    private String login;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<BookEntity> books;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(final String id) {
+        super(id);
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(final String login) {
+        this.login = login;
+    }
+
+    public List<BookEntity> getBooks() {
+        return books;
+    }
+
+    public void setBooks(final List<BookEntity> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "login='" + login + '\'' +
+                ", books=" + books +
+                '}';
+    }
+}
